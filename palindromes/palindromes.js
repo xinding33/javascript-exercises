640d655c79112881de3650a7a73cd31a0b6ed1ca
Xin Ding <xinding33@gmail.com>
2021-06-27 01:57:34 -0700

    Finished palindromes challenge

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function() {
+const palindromes = function(inputString) {
+    let originalString = inputString.toLowerCase().replace(/[^a-z]/g, '');
+    let flippedString = "";
 
+    for (let i = originalString.length - 1; i >= 0.; i--) {
+        flippedString += originalString[i];
+    }
+
+    return flippedString === originalString;
 }
 
 module.exports = palindromes
