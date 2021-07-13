1b3b3f07f9521903ed2a2297a2ec9d3f806c13ca
Xin Ding <xinding33@gmail.com>
2021-07-12 20:21:03 -0700

    Finished fibonacci exercise w/ recursive solution

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let nth = n;
+    if (typeof(nth) === "string") {
+        nth = parseInt(nth);
+    }
 
+    if (nth < 0) {
+        return "OOPS";
+    }
+    else {
+        return (nth < 3 ? 1 : fibonacci(nth - 1) + fibonacci(nth - 2))
+    }
 }
 
 module.exports = fibonacci
