f2589e624ff378c54deee533f4d203dc367efddc
Xin Ding <xinding33@gmail.com>
2021-07-12 21:41:37 -0700

    Finished findTheOldest with array.reduce function

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-let findTheOldest = function() {
+let findTheOldest = function(people) {
+    return people.reduce((oldestPerson, currentPerson) => {
+        let oldestAge = getAge(oldestPerson);
+        let currentAge = getAge(currentPerson);
+        return oldestAge > currentAge ? oldestPerson : currentPerson;
+    })
+}
 
+let getAge = function(person) {
+    return person.yearOfDeath == null ? new Date().getFullYear() - person.yearOfBirth : person.yearOfDeath - person.yearOfBirth;
 }
 
 module.exports = findTheOldest
